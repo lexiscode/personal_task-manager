77eb5bd812c6d948ac421f4fe4542905f12a8829
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-08-22 02:46:04 +0100

    feat: added Home and Client controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', HomeController::class);
+Route::resource('client', ClientContoller::class);
+
+
